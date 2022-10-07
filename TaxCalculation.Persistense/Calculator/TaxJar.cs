8e6618b7a6ddc8bf814522e085b121d46c1ca08e
Cedie <gabiknights06@gmail.com>
2022-10-07 17:21:58 +0800

    Add TaxCalculation.Persistense

---
+++ TaxCalculation.Persistense/Calculator/TaxJar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxCalculation.Core.Enumeration;
+using TaxCalculation.Core.Model;
+using TaxCalculation.Core.Strategy;
+
+namespace TaxCalculation.Persistent.Calculator
+{
+    public class TaxJar : ITaxCalculatorStrategy
+    {
+
+        public bool IsApplicable(TaxCalculatorOption option)
+        {
+            return option == TaxCalculatorOption.Tax_Jar;
+        }
+
+        public Task<Tax> CalculateTax(Order data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Rate> GetTaxRateByLocation(Location data)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
