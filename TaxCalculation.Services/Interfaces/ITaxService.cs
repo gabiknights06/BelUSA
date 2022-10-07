e11a1b1f8e03632c63fb509c6d607536de151c9b
Cedie <gabiknights06@gmail.com>
2022-10-07 17:23:12 +0800

    Add TaxCalculation.Service

---
+++ TaxCalculation.Services/Interfaces/ITaxService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxCalculation.Core.Model;
+
+namespace TaxCalculation.Services.Interfaces
+{
+    public interface ITaxService
+    {
+        Task<Rate> GetTaxRateByLocation(Location data);
+        Task<Tax> CalculateTax(Order data);
+    }
+}
---
+++ TaxCalculation.Services/TaxService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxCalculation.Core.Model;
+using TaxCalculation.Services.Interfaces;
+
+namespace TaxCalculation.Services
+{
+    public class TaxService : ITaxService
+    {
+        public Task<Tax> CalculateTax(Order data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Rate> GetTaxRateByLocation(Location data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
