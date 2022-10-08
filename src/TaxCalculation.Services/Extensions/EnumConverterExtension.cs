b191056ca1f5cbf713038c471c7c21ca506575bf
Cedie <gabiknights06@gmail.com>
2022-10-08 17:39:38 +0800

    Services: Create a EnumConverterExtension

---
+++ src/TaxCalculation.Services/Extensions/EnumConverterExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxCalculation.Persistent.Exceptions;
+
+namespace TaxCalculation.Services.Extensions
+{
+    public static class EnumConverterExtension
+    {
+        public static T ToEnum<T>(this string value) where T : struct
+        {
+            var result = Enum.TryParse(value, true, out T parsedValue);
+
+            if (!result)
+            {
+                throw new BadRequestException($"Invalid enum value of type '{typeof(T)}'");
+            }
+            return parsedValue;
+
+        }
+    }
+}
