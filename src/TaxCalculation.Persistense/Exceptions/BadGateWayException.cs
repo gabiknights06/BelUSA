d4e6358a98f2e6bc7241197ef5ece54db5c4b8c6
Cedie <gabiknights06@gmail.com>
2022-10-08 14:47:34 +0800

    Persistense: Add exception classes

---
+++ src/TaxCalculation.Persistense/Exceptions/BadGateWayException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculation.Persistent.Exceptions
+{
+    public class BadGateWayException : Exception
+    {
+        public BadGateWayException() { }
+        public BadGateWayException(string message) : base(message) { }
+        public BadGateWayException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ src/TaxCalculation.Persistense/Exceptions/ValidatorException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculation.Persistent.Exceptions
+{
+    public class ValidatorException : Exception
+    {
+        public ValidatorException() { }
+        public ValidatorException(string message) : base(message) { }
+        public ValidatorException(string message, Exception inner) : base(message, inner) { }
+    }
+}
